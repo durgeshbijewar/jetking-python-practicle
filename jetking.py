924cb7b76f8aa3c604179b0ef55b1d15a97757ab
durgeshbijewar <137867028+durgeshbijewar@users.noreply.github.com>
2023-07-19 13:18:34 +0530

    Add files via upload

---
+++ jetking.py
@@ -0,0 +1,45 @@
+def reverse_vowels(string):
+vowels = ""
+
+
+    return str(n) is strfor character in string:
+    if character in "aeiouAEIOU"
+    vowels = vowels + character
+result_string = ""
+
+for character in string:
+    if character in "aeiouAEIOU":
+        result_string = result_string + vowels[-1]
+        vowels = vowels[:-1]
+    else:
+        result_string = result_string + character
+        return result_string
+    
+    print(reverse_vowels("durges"))
+    print(reverse_vowels("aeiouAEIOU"))
+    print(reverse_vowels("blockchain"))
+    print(reverse_vowels("bijewar"))
+
+#dublicate element remove
+
+def remove_dublicate(numbers):
+    for i in range(len(numbers)-1,0,-1):
+        if numbers[i] ++ numbers[i -1]:
+            del numbers[i +1]
+            return len(numbers)
+        
+ print(remove_dublicate([1,1,2,3,4,4,4,0,0,0,0,0,4]))
+
+
+#write a program tp chek wheather a givn intiger is palidrome or not
+
+def is_palindrome(n):(n) [i:-1]
+
+print(is_palindrome(100))
+print(is_palindrome(263))
+print(is_palindrome(453))
+
+
+
+
+
